d92db6da6d0bf2d0804590e9fdc19f1b150ebcfc
githubhuangyaohui <persistenthuang@163.com>
2020-12-08 17:53:57 +0800

    添加公共样式

--- src/App.vue
+++ src/App.vue
@@ -23,29 +23,4 @@ body,html {
     padding: 0;
     border: none;
 }
-.back-image{
-    background: url("http://pic1.win4000.com/wallpaper/2020-11-27/5fc0c6c7406ac.jpg");
-    /*background: #DCDFE6;*/
-    background-size: 100%;
-}
-.search{
-    background: #409EFF;
-    min-height: 90px;
-    margin: 10px;
-    padding: 0px;
-}
-.search-item{
-    width: 200px;
-    float: left;
-    margin: 10px;
-}
-.search-item:hover {
-    margin-top: -2px;
-}
-.main{
-    background: #909399;
-    min-height: 500px;
-    margin: 10px;
-    padding: 0px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import './base.css'
 
 Vue.config.productionTip = false
 var axios = require('axios')
